e6a61b82cf975b804d771db828cec124333031d9
cacerftw <cacerftw@hotmail.com>
2017-05-18 17:09:28 +0100

    dia 18 v2

--- app/view/main/tickets/enviarespostas/EnviaMail/mandarmail.php
+++ app/view/main/tickets/enviarespostas/EnviaMail/mandarmail.php
@@ -1,11 +1,6 @@
 ﻿<?php
-include("config.php");
 require 'class.smtp.php';
 require 'class.phpmailer.php';
-
-
-$assunto = $_POST['assuntoresposta'];
-$conteudo = $_POST['conteudoresposta'];
 $PHPMailer = new PHPMailer();
 
 // define que será usado SMTP
@@ -17,23 +12,13 @@ $PHPMailer->isHTML( true );
 // codificação UTF-8, a codificação mais usada recentemente
 $PHPMailer->Charset = 'UTF-8';
 
-$cookieEmail = $_COOKIE['cookieEmail'];
-//selecting data associated with this particular id
-$result = mysqli_query($mysqli, "SELECT * FROM admin WHERE username='$cookieEmail'") or die(mysqli_error($mysqli));
-
-while($res = mysqli_fetch_array($result))
-{
-  $username = $res['username'];
-	$password = $res['pass'];
-}
-
 // Configurações do SMTP
 $PHPMailer->SMTPAuth = true;
 $PHPMailer->SMTPSecure = 'TLS';
 $PHPMailer->Host = 'smtp.gmail.com';
 $PHPMailer->Port = 587;
-$PHPMailer->Username = $username;
-$PHPMailer->Password = $password;
+$PHPMailer->Username = 'testetrackit@gmail.com';
+$PHPMailer->Password = 'testetrackit123';
 
 // E-Mail do remetente (deve ser o mesmo de quem fez a autenticação
 // nesse caso seu_login@gmail.com)
@@ -43,26 +28,18 @@ $PHPMailer->From = 'testetrackit@gmail.com';
 $PHPMailer->FromName = 'teste trackit';
 
 // assunto da mensagem
-$PHPMailer->Subject = $assunto;
+$PHPMailer->Subject = 'aaa';
 
 // corpo da mensagem
-$PHPMailer->Body = $conteudo;
+$PHPMailer->Body = '<p>Mensagem em HTML</p>';
 
 // corpo da mensagem em modo texto
-// $PHPMailer->AltBody = 'Mensagem em texto';
+$PHPMailer->AltBody = 'Mensagem em texto';
 
-$cookieID = $_COOKIE['cookieID'];
-//selecting data associated with this particular id
-$result2 = mysqli_query($mysqli, "SELECT * FROM emails WHERE id=$cookieID") or die(mysqli_error($mysqli));
-
-while($res = mysqli_fetch_array($result))
-{
-  $fromaddress = $res['fromaddress'];
-}
 
 // adiciona destinatário (pode ser chamado inúmeras vezes)
-$PHPMailer->AddReplyTo($fromaddress);
-$PHPMailer->AddAddress($fromaddress);
+$PHPMailer->AddReplyTo('cacerftw@hotmail.com', 'Nome do visitante');
+$PHPMailer->AddAddress( 'cacerftw@hotmail.com' );
 
 
 // verifica se enviou corretamente
