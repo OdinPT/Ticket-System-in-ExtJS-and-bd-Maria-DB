7671148ff292c952eccebf32201f9de06f2a8e19
OdinPT <leonardo.almeidavieira@gmail.com>
2017-05-25 09:36:57 +0100

    sas

--- app/php/Apagar/apagarfuncionario.php
+++ app/php/Apagar/apagarfuncionario.php
@@ -3,5 +3,5 @@ include("config.php");
 $id = $_COOKIE['cookieIDfuncionario'];
 
 //$kappa = mysqli_query($mysqli, "DELETE FROM respostas where id=$id");
-kappa = mysqli_query($mysqli, "DELETE FROM funcionarios WHERE id=$id");
+kappa = mysqli_query($mysqli, "DELETE FROM funcionario WHERE id=$id");
 ?>
--- app/view/admin/EditaUtilizadorController.js
+++ app/view/admin/EditaUtilizadorController.js
@@ -9,7 +9,7 @@ Ext.define('TrackIT.view.admin.EditaUtilizadorController', {
                 url: 'app/php/Editar/editautilizador.php',
                 success: function (response, opts){Ext.MessageBox.alert('Sucesso','Utilizador Editado!');},
                 failure: function (){alert('Erro...');},
-                params: { user: Ext.getCmp('user2').getValue(), pass: Ext.getCmp('password2').getValue(), id_departamento: Ext.getCmp('id_departamento2').getValue(), tipo_funcionario: Ext.getCmp('tipo_funcionario2').getValue()}
+                params: { username: Ext.getCmp('user2').getValue(), pass: Ext.getCmp('password2').getValue(), id_departamento: Ext.getCmp('id_departamento2').getValue(), tipo_funcionario: Ext.getCmp('tipo_funcionario2').getValue()}
             });
 
     }
